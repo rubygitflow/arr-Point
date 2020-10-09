fd59266077f26b354dfc6da41e5f731cfa710906
rubygitflow <nrubygitflow@gmail.com>
2020-10-09 13:21:07 +0300

    fix remote access 4

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module ArrPoint
     config.load_defaults 6.0
 
     config.time_zone = 'Moscow'
-    config.i18n.default_locale = :ru # == 2gis.default_ocale = :ru
+    config.i18n.default_locale = :ru # == 2gis.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
